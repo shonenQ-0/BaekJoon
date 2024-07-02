5cf6c778f581d959fd5ab38880e65b0956db6dd4
Hyeongyu Son <shonenq00@gmail.com>
2024-07-02 18:21:46 +0900

    나이순 정렬

--- src/B10814.java
+++ src/B10814.java
@@ -1,10 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class B10814 {
     public static void main(String[] args) throws IOException {
