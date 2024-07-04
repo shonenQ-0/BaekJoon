4d0cb71166ff79e6ecc9fbb53af03d45dc2a1660
Hyeongyu Son <shonenq00@gmail.com>
2024-07-04 19:24:53 +0900

    서로 다른 부분 문자열의 개수

---
+++ src/B11478.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.TreeSet;
+
+public class B11478 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = br.readLine();
+        
+        HashSet<String> hs = new HashSet<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = 1; i + j <= s.length() ; j++) {
+                String tmp = s.substring(i, j + i);
+               // System.out.println(tmp);
+                hs.add(tmp);
+            }
+            
+        }
+        System.out.println(hs.size());
+    }
+}
+
+
+/*
+        while(len != 0){
+            tmp = "";
+            for (int i = 0; i < s.length(); i++) {
+                for (int j = 0; j < s.length(); j++) {
+                    tmp += s.charAt(j);
+                    if (tmp.length() == len) {
+                        System.out.println(tmp);
+                        hs.add(tmp);
+                        tmp = "";
+                        
+                    }
+                }
+            }
+            len--;
+        }
+
+
+ */
+ 
