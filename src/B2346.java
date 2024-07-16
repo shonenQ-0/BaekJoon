48807edd3adff8b65ab838970cd05bc97b6863ca
Hyeongyu Son <shonenq00@gmail.com>
2024-07-17 00:02:01 +0900

    Deque를 사용해 앞과 뒤에서 인자를 추출해가며 문제를 해결했다. /  Deque<Integer> d = new ArrayDeque<>();를 LinkedList로 구현했을때 메모리초과문제를 겪었다.

--- src/B2346.java
+++ src/B2346.java
@@ -3,7 +3,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.Deque;
-import java.util.LinkedList;
 import java.util.StringTokenizer;
 
 public class B2346 {
